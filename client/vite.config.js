7a400f7594f4c1a4a0b0947c8aaf0b5b649e8a19
Karan <kraorane020@gmail.com>
2026-03-09 17:05:54 +0530

    conflect probelem resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: [
+        path.resolve('src'),
+        path.resolve('.svelte-kit'),
+        path.resolve('node_modules'),
+        'D:/Project'
+      ]
+    }
+  }
 });
